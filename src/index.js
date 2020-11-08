3831d9209509e58aea031b7a9dbf7021b717f4cf
Nischal Shrestha <nischalstha9@gmail.com>
2020-11-08 18:56:22 +0545

    better loading

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api/portfolio/';
+axios.defaults.baseURL = 'https://nischalstha9.pythonanywhere.com/api/portfolio/';
 
 ReactDOM.render(
   <React.StrictMode>
