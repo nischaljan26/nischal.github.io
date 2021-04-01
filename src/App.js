100ac889de9bbc3f1939ec6200017020c3e02b77
nischal shrestha <nischalstha9@gmail.com>
2021-04-01 20:54:43 +0545

    dynamic contacts

--- src/App.js
+++ src/App.js
@@ -1,81 +1,92 @@
-import './App.css';
+import "./App.css";
 import { HashRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/navbar"
-import About from "./components/about"
-import Experience from "./components/experience"
-import Education from "./components/Education"
-import Skills from "./components/Skills"
-import Interests from "./components/Interests"
-import MyProjects from "./components/MyProjects"
-import AddMe from "./components/AddMe"
-import {useState, useEffect} from 'react';
-import axios from 'axios';
-import loading from './assets/loading.gif';
-
+import Navbar from "./components/navbar";
+import About from "./components/about";
+import Experience from "./components/experience";
+import Education from "./components/Education";
+import Skills from "./components/Skills";
+import Interests from "./components/Interests";
+import MyProjects from "./components/MyProjects";
+import AddMe from "./components/AddMe";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import loading from "./assets/loading.gif";
 
 function App() {
-    const [firstName, setfirstName] = useState('')
-    const [lastName, setlastName] = useState('')
-    const [DP, setDp] = useState('')
-    const [street, setstreet] = useState('')
-    const [address, setaddress] = useState('')
-    const [shortInfo, setshortInfo] = useState('')
-    const [contactQR, setcontactQR] = useState('')
-    const [CV, setCV] = useState('')
-    const [interest, setinterest] = useState('')
-    const [expRemark, setExpRemark] = useState('')
-    const [isloading, setisLoading] = useState(true)
-    
-    useEffect(() => {
-        const fetchAbout = async () => {
-            const result = await axios('about')
-            const obj = result.data[0]
-            try {
-              setfirstName(obj.first_name);
-              setlastName(obj.last_name);
-              setDp(obj.dp);
-              setstreet(obj.address_street);
-              setaddress(obj.full_address);
-              setshortInfo(obj.short_description);
-              setCV(obj.cv);
-              setcontactQR(obj.contact_qr);
-              setinterest(obj.interests);
-              setExpRemark(obj.experience_remarks);
-              setisLoading(false);
-            } catch (error) {
-              console.log(error)
-            }
-        }
-        fetchAbout()
-      }, [])
-  return (
-    isloading?
-    <div className='loading-home-container'>
-      <img src={loading} alt='Loading...' className='loading-home'/>
+  const [firstName, setfirstName] = useState("");
+  const [lastName, setlastName] = useState("");
+  const [DP, setDp] = useState("");
+  const [street, setstreet] = useState("");
+  const [address, setaddress] = useState("");
+  const [shortInfo, setshortInfo] = useState("");
+  const [contactQR, setcontactQR] = useState("");
+  const [CV, setCV] = useState("");
+  const [interest, setinterest] = useState("");
+  const [expRemark, setExpRemark] = useState("");
+  const [isloading, setisLoading] = useState(true);
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const fetchAbout = async () => {
+      const result = await axios("about");
+      const obj = result.data[0];
+      try {
+        setfirstName(obj.first_name);
+        setlastName(obj.last_name);
+        setDp(obj.dp);
+        setstreet(obj.address_street);
+        setaddress(obj.full_address);
+        setshortInfo(obj.short_description);
+        setCV(obj.cv);
+        setcontactQR(obj.contact_qr);
+        setinterest(obj.interests);
+        setExpRemark(obj.experience_remarks);
+        setisLoading(false);
+        setContacts(obj.contacts);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAbout();
+  }, []);
+  return isloading ? (
+    <div className="loading-home-container">
+      <img src={loading} alt="Loading..." className="loading-home" />
     </div>
-    :
+  ) : (
     <Router>
-      <Navbar contactQr={contactQR} dp={DP} cv={CV} firstName={firstName} lastName={lastName}/>
+      <Navbar dp={DP} cv={CV} firstName={firstName} lastName={lastName} />
       <div className="container-fluid p-0">
-        <Route path = "/" 
-        exact 
-        component={() => 
-          <About
-          DP={DP}
-          firstName={firstName}
-          lastName={lastName}
-          street={street}
-          address={address}
-          shortInfo={shortInfo}
-          />
-          }        
+        <Route
+          path="/"
+          exact
+          component={() => (
+            <About
+              DP={DP}
+              firstName={firstName}
+              lastName={lastName}
+              street={street}
+              address={address}
+              shortInfo={shortInfo}
+              contacts={contacts}
+            />
+          )}
         />
-        <Route path = "/experience" component={()=><Experience expRemark={expRemark}/>} />
-        <Route path = "/education" component={Education} />
-        <Route path = "/addme" component={() => <AddMe contactQr={contactQR} />} />
-        <Route path = "/my-projects" component={MyProjects} />
-        <Route path = "/interests" component={() => <Interests interest={interest}/>} />
-        <Route path = "/skills" component={Skills} />
+        <Route
+          path="/experience"
+          component={() => <Experience expRemark={expRemark} />}
+        />
+        <Route path="/education" component={Education} />
+        <Route
+          path="/addme"
+          component={() => <AddMe contactQr={contactQR} />}
+        />
+        <Route path="/my-projects" component={MyProjects} />
+        <Route
+          path="/interests"
+          component={() => <Interests interest={interest} />}
+        />
+        <Route path="/skills" component={Skills} />
       </div>
     </Router>
   );
--- src/components/about.js
+++ src/components/about.js
@@ -1,28 +1,45 @@
-import React from 'react';
-import SocialIcon from './partials/socialIcon'
+import React from "react";
+import SocialIcon from "./partials/socialIcon";
 
-const About = ({DP, firstName, lastName, street, address, shortInfo}) => {
-    return (
-        <div>
-            <section className="resume-section" id="about">
-                <div className="resume-section-content">
-                    <span className="d-sm-block d-md-block d-lg-none">
-                        <img className="img-fluid img-profile rounded-circle mx-auto mb-2" style={{maxWidth: '200px'}} src={DP} alt="" />
-                    </span>
-                    <div className="js-scroll-trigger"><h1 className="mb-0">{firstName} <span className="text-primary">{lastName}</span></h1></div>
-                    <div className="subheading mb-5">{street} · {address} <br/>
-                     (+977) 9843481901 · 9810374388<br/>
-                     <a href="mailto:nischalstha9@gmail.com">
-                         nischalstha9@gmail.com
-                     </a>
-                    </div>
-                    <p className="lead mb-5">{shortInfo}</p>
-                    <SocialIcon/>
-                </div>
-            </section>
-            <hr className="m-0" />
+const About = ({
+  DP,
+  firstName,
+  lastName,
+  street,
+  address,
+  shortInfo,
+  contacts,
+}) => {
+  return (
+    <div>
+      <section className="resume-section" id="about">
+        <div className="resume-section-content">
+          <span className="d-sm-block d-md-block d-lg-none">
+            <img
+              className="img-fluid img-profile rounded-circle mx-auto mb-2"
+              style={{ maxWidth: "200px" }}
+              src={DP}
+              alt=""
+            />
+          </span>
+          <div className="js-scroll-trigger">
+            <h1 className="mb-0">
+              {firstName} <span className="text-primary">{lastName}</span>
+            </h1>
+          </div>
+          <div className="subheading mb-5">
+            {street} · {address} <br />
+            (+977) {contacts.join(" · ")}
+            <br />
+            <a href="mailto:nischalstha9@gmail.com">nischalstha9@gmail.com</a>
+          </div>
+          <p className="lead mb-5">{shortInfo}</p>
+          <SocialIcon />
         </div>
-    )
-}
+      </section>
+      <hr className="m-0" />
+    </div>
+  );
+};
 
-export default About
+export default About;
